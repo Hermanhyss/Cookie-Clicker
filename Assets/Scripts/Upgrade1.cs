46f6dfeced4fc956c0c5f232c8a0e2044a7b9feb
Belgacem Gmir <belgacem.gmir@learnet.se>
2024-10-10 10:48:10 +0200

    .

--- Assets/Scripts/Upgrade1.cs
+++ Assets/Scripts/Upgrade1.cs
@@ -2,7 +2,16 @@ using UnityEngine;
 
 public class Upgrade1 : MonoBehaviour
 {
-   
+
+    [SerializeField] int cookieCount = 0;
+    [SerializeField] int CookieAdd = 1;
+
+
+    public void 
+    
+    
+    
+    
     void Start()
     {
         
