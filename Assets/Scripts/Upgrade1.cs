00247d354d65c665a389d228383c5d13a8f17ed4
Belgacem Gmir <belgacem.gmir@learnet.se>
2024-10-14 16:13:03 +0200

    .

--- Assets/Scripts/Upgrade1.cs
+++ Assets/Scripts/Upgrade1.cs
@@ -6,11 +6,6 @@ public class Upgrade1 : MonoBehaviour
     [SerializeField] int cookieCount = 0;
     [SerializeField] int CookieAdd = 1;
 
-
-    public void 
-    
-    
-    
     
     void Start()
     {
